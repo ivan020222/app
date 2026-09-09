4870a12239aaa2aa12d1a445735288093883885a
Ivan Vovk <vovki.2105@gmail.com>
2026-09-09 10:30:25 +0300

    Right CORS placement

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-
+  
   app.enableCors({
     origin: process.env.FRONTEND_URL || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
+  await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
