5064f0bd46e9b1ba1af06a14b70ffe52d0aa4eb9
Ivan Vovk <vovki.2105@gmail.com>
2026-09-09 08:57:15 +0300

    Cors adding commit

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
 }
 await bootstrap();
-
-app.enableCors({
- origin: process.env.FRONTEND_URL || '*',
- methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-});
